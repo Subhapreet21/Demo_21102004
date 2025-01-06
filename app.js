6e23b9fd80186b5c647c37712ba30a76ed5f549b
Subhapreet21 <subhapreetpatro2004@gmail.com>
2025-01-06 13:20:01 +0530

    Pushing the files to repo

---
+++ app.js
@@ -0,0 +1,55 @@
+// // console.log(10);
+// // document.write(1000);
+
+// // var a = 10;
+// // a = "hi"; // Re-intialization
+// // console.log(a);
+
+// var user = "Mahesh";
+// var user = "Ramesh"; // Re-declaration
+// console.log(user);
+
+// let a = 10;
+// a = 100;
+// console.log(a);
+
+// // let b = 20;
+// // let b = 200;
+// // console.log(b); // Error
+
+// const dob = 2004;
+// // dob = 2005; // Error
+// // const dob = 2005; // Error
+// console.log(dob);
+
+// let str1 = "doublequotes";
+// console.log(str1);
+// console.log(typeof str1);
+
+// let str2 = "singlequotes";
+// console.log(str2);
+// console.log(typeof str2);
+
+// let str3 = `backticks`;
+// console.log(str3);
+// console.log(typeof str3);
+
+// let num1 = 10;
+// console.log(num1, typeof num1);
+
+// let num2 = 10.5;
+// console.log(num2, typeof num2);
+
+// let bool1 = true;
+// console.log(bool1, typeof bool1);
+
+// let bool2 = false;
+// console.log(bool2, typeof bool2);
+
+// let a;
+// console.log(a, typeof a);
+
+// let a = null;
+// console.log(a, typeof a);
+
+// console.log(10n, typeof 10n);
