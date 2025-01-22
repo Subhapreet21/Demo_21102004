f075504be0c39604aabe2360bbe86662fb8eab93
Subhapreet21 <subhapreetpatro2004@gmail.com>
2025-01-22 12:55:05 +0530

    API Tracking

---
+++ async.js
@@ -0,0 +1,64 @@
+console.log(10);
+console.log(20);
+setTimeout(() => {
+  console.log(30);
+}, 5000);
+setInterval(() => {
+  //   document.writeln("Malla Reddy Unviersity");
+  //   console.lOg(30);
+}, 1500);
+console.log(40);
+console.log(50);
+
+// !Promise
+
+// let p1 = new Promise((resolve, reject) => {});
+// console.log(p1);
+
+let p2 = new Promise((resolve, reject) => {
+  resolve("success");
+});
+// console.log(p2);
+p2.then((response) => console.log(response))
+  .catch((error) => console.log(error))
+  .finally(() => console.log("Finally for both"));
+
+let p3 = new Promise((resolve, reject) => {
+  reject("failure");
+});
+// console.log(p3);
+
+p3.then((data) => console.log(data))
+  .catch((err) => console.log(err))
+  .finally(() => console.log("Finally for both"));
+
+// ! API Fetching
+
+// https://jsonplaceholder.typicode.com/users
+
+function fetchUsers() {
+  let response = fetch("https://jsonplaceholder.typicode.com/users");
+  // console.log(response);
+  response
+    .then((result) => {
+      // console.log(result.json());
+      return result.json().then((data) => {
+        // console.log(data);
+        let store = document.getElementById("store");
+        // console.log(store);
+        data.map((user) => {
+          // console.log(user);
+          store.innerHTML += `
+          <tr>
+            <td>${user.id}</td>
+            <td>${user.name}</td>            
+            <td>${user.email}</td>
+            <td>${user.company.name}</td>
+          </tr>
+          `;
+        });
+      });
+    })
+    .catch((err) => console.log(err));
+}
+fetchUsers();
