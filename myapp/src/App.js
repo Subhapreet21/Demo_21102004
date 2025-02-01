58c7a57da0cbce0842738c1ada51c929fdf12d73
Subhapreet21 <subhapreetpatro2004@gmail.com>
2025-02-01 12:54:31 +0530

    React Installation and Setup

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -2,7 +2,6 @@ function App() {
   return (
     <div className="App">
       <h1 style={{ color: "red" }}>
-        {" "}
         One hour is wasted in rectifying your errors
       </h1>
     </div>
