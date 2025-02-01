e0989cc3a69c7643ee195340bdb153671f2607ab
Subhapreet21 <subhapreetpatro2004@gmail.com>
2025-02-01 12:55:34 +0530

    React Installation and Setup

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,9 +1,7 @@
 function App() {
   return (
     <div className="App">
-      <h1 style={{ color: "red" }}>
-        One hour is wasted in rectifying your errors
-      </h1>
+      <h1 style={{ color: "red" }}>Name: Subhapreet Patro</h1>
     </div>
   );
 }
